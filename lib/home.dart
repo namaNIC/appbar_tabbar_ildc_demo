18c0eb54a61e14081065a9a0dab638c8c08118a2
Naman Vaishnav <naman.v@indianic.com>
2021-09-22 10:45:34 +0530

    Drawer Demo completed

--- lib/home.dart
+++ lib/home.dart
@@ -13,11 +13,81 @@ class MyBar extends AppBar {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var _scafoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scafoldKey,
       appBar: MyBar(
         title: Text("Home"),
+      ),
+      endDrawer: DrawerClass(),
+      drawer: DrawerClass(),
+      body: InkWell(
+        onTap: () {
+          // _scafoldKey.currentState?.openDrawer();
+          _scafoldKey.currentState?.openEndDrawer();
+          print("Clicked");
+        },
+        child: Center(
+          child: Container(
+            width: 100,
+            height: 50,
+            color: Colors.red,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerClass extends StatelessWidget {
+  const DrawerClass({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.blue,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Container(
+              color: Colors.red,
+              height: 40,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.yellow,
+              height: 40,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              color: Colors.purple,
+              height: 40,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Spacer(),
+            Container(
+              color: Colors.amber,
+              height: 40,
+              child: Text("Logout"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
